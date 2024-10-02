8afbdd4cb4d5011eb11406c4e02164c97d1ec72d
Álvaro <alvarocc948@gmail.com>
2024-10-02 10:47:50 +0200

    Cambio variables del registro

--- Inventario/src/main/java/ui/register.java
+++ Inventario/src/main/java/ui/register.java
@@ -50,55 +50,55 @@ public class register {
 		frmRegistroDeUsuarios.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmRegistroDeUsuarios.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Registro de usuarios");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel.setBounds(10, 11, 151, 14);
-		frmRegistroDeUsuarios.getContentPane().add(lblNewLabel);
+		JLabel titleRegister = new JLabel("Registro de usuarios");
+		titleRegister.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		titleRegister.setBounds(10, 11, 151, 14);
+		frmRegistroDeUsuarios.getContentPane().add(titleRegister);
 		
-		JLabel lblNewLabel_1 = new JLabel("Nombre");
-		lblNewLabel_1.setBounds(10, 55, 46, 14);
-		frmRegistroDeUsuarios.getContentPane().add(lblNewLabel_1);
+		JLabel userName = new JLabel("Nombre");
+		userName.setBounds(10, 55, 46, 14);
+		frmRegistroDeUsuarios.getContentPane().add(userName);
 		
 		textField = new JTextField();
 		textField.setBounds(124, 52, 140, 20);
 		frmRegistroDeUsuarios.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("Email");
-		lblNewLabel_2.setBounds(10, 80, 46, 14);
-		frmRegistroDeUsuarios.getContentPane().add(lblNewLabel_2);
+		JLabel userMail = new JLabel("Email");
+		userMail.setBounds(10, 80, 46, 14);
+		frmRegistroDeUsuarios.getContentPane().add(userMail);
 		
 		textField_1 = new JTextField();
 		textField_1.setBounds(124, 77, 140, 20);
 		frmRegistroDeUsuarios.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
-		JLabel lblNewLabel_3 = new JLabel("Contraseña");
-		lblNewLabel_3.setBounds(10, 118, 68, 14);
-		frmRegistroDeUsuarios.getContentPane().add(lblNewLabel_3);
+		JLabel userPassword = new JLabel("Contraseña");
+		userPassword.setBounds(10, 118, 68, 14);
+		frmRegistroDeUsuarios.getContentPane().add(userPassword);
 		
 		passwordField = new JPasswordField();
 		passwordField.setBounds(124, 115, 140, 20);
 		frmRegistroDeUsuarios.getContentPane().add(passwordField);
 		
-		JLabel lblNewLabel_4 = new JLabel("Confirmar contraseña");
-		lblNewLabel_4.setBounds(10, 143, 126, 20);
-		frmRegistroDeUsuarios.getContentPane().add(lblNewLabel_4);
+		JLabel userPasswordConfirm = new JLabel("Confirmar contraseña");
+		userPasswordConfirm.setBounds(10, 143, 126, 20);
+		frmRegistroDeUsuarios.getContentPane().add(userPasswordConfirm);
 		
 		passwordField_1 = new JPasswordField();
 		passwordField_1.setBounds(124, 143, 140, 20);
 		frmRegistroDeUsuarios.getContentPane().add(passwordField_1);
 		
-		JButton btnNewButton = new JButton("Registrarse");
-		btnNewButton.setBounds(211, 227, 89, 23);
-		frmRegistroDeUsuarios.getContentPane().add(btnNewButton);
+		JButton registerButton = new JButton("Registrarse");
+		registerButton.setBounds(211, 227, 89, 23);
+		frmRegistroDeUsuarios.getContentPane().add(registerButton);
 		
-		JButton btnNewButton_1 = new JButton("Iniciar sesión");
-		btnNewButton_1.setBounds(10, 227, 111, 23);
-		frmRegistroDeUsuarios.getContentPane().add(btnNewButton_1);
+		JButton loginButton = new JButton("Iniciar sesión");
+		loginButton.setBounds(10, 227, 111, 23);
+		frmRegistroDeUsuarios.getContentPane().add(loginButton);
 		
-		JLabel lblNewLabel_5 = new JLabel("¿Ya tienes cuenta?");
-		lblNewLabel_5.setBounds(10, 208, 164, 14);
-		frmRegistroDeUsuarios.getContentPane().add(lblNewLabel_5);
+		JLabel infoLogin = new JLabel("¿Ya tienes cuenta?");
+		infoLogin.setBounds(10, 208, 164, 14);
+		frmRegistroDeUsuarios.getContentPane().add(infoLogin);
 	}
 }
